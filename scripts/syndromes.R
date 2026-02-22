4337de20a590deaafddb7f16704b719a527ba2b8
eliotmonaco <eliotmonaco@gmail.com>
2026-02-21 20:31:09 -0600

    add syndromes identified by MODHSS

--- scripts/syndromes.R
+++ scripts/syndromes.R
@@ -1,76 +1,115 @@
-# Syndrome list to use in `build_url()`
+# Essence syndrome queries for API
 
 syn <- list(
-  # Syndromic
-  abd = paste0(
-    "medicalGroupingSystem=chiefcomplaintsubsyndromes&",
-    "medicalGrouping=abdominalpain"
+  hepb = list(
+    queryname = "Acute Hepatitis B v1",
+    apistring = "ccddCategory=acute%20hepatitis%20b%20v1"
   ),
-  drh = paste0(
-    "medicalGroupingSystem=chiefcomplaintsubsyndromes&",
-    "medicalGrouping=diarrhea"
+  resp = list(
+    queryname = "CDC Broad Acute Respiratory DD v1",
+    apistring = "ccddCategory=cdc%20broad%20acute%20respiratory%20dd%20v1"
   ),
-  fev = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "medicalGrouping=fever"
+  cpox = list(
+    queryname = "CDC Chickenpox v2",
+    apistring = "ccddCategory=cdc%20chickenpox%20v2"
   ),
-  gi = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "medicalGrouping=gi"
+  covid = list(
+    queryname = "CDC COVID-Specific DD v1",
+    apistring = "ccddCategory=cdc%20covid-specific%20dd%20v1"
   ),
-  neuro = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "medicalGrouping=neuro"
+  food = list(
+    queryname = "CDC Food Poisoning v1",
+    apistring = "ccddCategory=cdc%20food%20poisoning%20v1"
   ),
-  rash = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "medicalGrouping=rash"
+  hepa = list(
+    queryname = "CDC Hepatitis A v1",
+    apistring = "ccddCategory=cdc%20hepatitis%20a%20v1"
   ),
-  resp = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "medicalGrouping=resp"
+  ili = list(
+    queryname = "ILI CCDD v1",
+    apistring = "ccddCategory=ili%20ccdd%20v1"
   ),
-  vom = paste0(
-    "medicalGroupingSystem=chiefcomplaintsubsyndromes&",
-    "medicalGrouping=vomiting"
+  meas = list(
+    queryname = "CDC Measles CCDD v1",
+    apistring = "ccddCategory=cdc%20measles%20ccdd%20v1"
   ),
-  # Respiratory
-  cov = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "ccddCategory=cdc%20covid-specific%20dd%20v1"
+  mpox = list(
+    queryname = "CDC Monkeypox DD v1",
+    apistring = "ccddCategory=cdc%20monkeypox%20dd%20v1"
   ),
-  ili = paste0(
-    "medicalGroupingSystem=chiefcomplaintsubsyndromes&",
-    "medicalGrouping=ili"
+  pert = list(
+    queryname = "Pertussis v2 CCDD Parsed",
+    apistring = "ccddCategory=pertussis%20v2%20ccdd%20parsed"
   ),
-  rsv = paste0( # CDC RSV query + croup
-    "medicalGroupingSystem=essencesyndromes&",
-    "ccddCategoryFreeText=%5E%20RSV%20%5E,or,%5Ebronchiolitis%5E,or,%5Esyncytial%20virus%5E,or,%5Ecroup%5E,or,%20%5E%5B;/%5DB97.4%5E,or,%5E%5B;/%5DB974%5E,or,%20%5E%5B;/%5DJ12.1%5E,or,%5E%5B;/%5DJ121%5E,or,%20%5E%5B;/%5DJ20.5%5E,or,%5E%5B;/%5DJ205%5E,or,%20%5E%5B;/%5DJ21.0%5E,or,%5E%5B;/%5DJ210%5E,or,%20%5E%5B;/%5D466.11%5E,or,%5E%5B;/%5D46611%5B;/%5D%5E,or,%20%5E%5B;/%5D480.1%5E,or,%5E%5B;/%5D4801%5B;/%5D%5E,or,%20%5E%5B;/%5D079.6%5E,or,%5E%5B;/%5D0796%5B;/%5D%5E,or,%20%5E%5B;/%5D55735004%5B;/%5D%5E,or,%20%5E%5B;/%5D408684006%5B;/%5D%5E,or,%20%5E%5B;/%5D195881003%5B;/%5D%5E,or,%20%5E%5B;/%5D10625551000119103%5B;/%5D%5E,or,%20%5E%5B;/%5D57089007%5B;/%5D%5E,or,%20%5E%5B;/%5D195739001%5B;/%5D%5E,or,%20%5E%5B;/%5D79479005%5B;/%5D%5E,or,%20%5E%5B;/%5D195727009%5B;/%5D%5E,or,%20%5E%5B;/%5D72204002%5B;/%5D%5E,or,%20%5E%5B;/%5D31309002%5B;/%5D%5E%09%09"
+  rsv = list(
+    queryname = "CDC Respiratory Syncytial Virus v1",
+    apistring = "ccddCategory=cdc%20respiratory%20syncytial%20virus%20v1"
   ),
-  # Bioterrorism
-  anth = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "ccddCategory=anthrax%20v1"
+  shig = list(
+    queryname = "CDC Shigella v1",
+    apistring = "ccddCategory=cdc%20shigella%20v1"
   ),
-  bot = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "medicalGrouping=bot_like"
+  spox = list(
+    queryname = "CDC Small Pox v1",
+    apistring = "ccddCategory=cdc%20small%20pox%20v1"
   ),
-  plg = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "ccddCategory=plague%20v1"
+  hus = list(
+    queryname = "Hemolytic Uremic Syndrome (HUS) v1",
+    apistring = "ccddCategory=hemolytic%20uremic%20syndrome%20(hus)%20v1"
   ),
-  smpx = paste0(
-    "medicalGroupingSystem=chiefcomplaintsubsyndromes&",
-    "medicalGrouping=smallpox"
+  men = list(
+    queryname = "Meningococcal Disease v1",
+    apistring = "ccddCategory=meningococcal%20disease%20v1"
   ),
-  tul = paste0(
-    "medicalGroupingSystem=chiefcomplaintsubsyndromes&",
-    "medicalGrouping=tularemia"
+  mump = list(
+    queryname = "Mumps v1",
+    apistring = "ccddCategory=mumps%20v1"
   ),
-  vhf = paste0(
-    "medicalGroupingSystem=essencesyndromes&",
-    "medicalGrouping=hemr_ill"
+  noro = list(
+    queryname = "Norovirus v1",
+    apistring = "ccddCategory=norovirus%20v1"
+  ),
+  drug = list(
+    queryname = "CDC All Drug Overdose v3 Parsed",
+    apistring = "ccddCategory=cdc%20all%20drug%20overdose%20v3%20parsed"
+  ),
+  alc = list(
+    queryname = "CDC Alcohol v1",
+    apistring = "ccddCategory=cdc%20alcohol%20v1"
+  ),
+  fent = list(
+    queryname = "CDC Fentanyl Overdose v2 Parsed",
+    apistring = "ccddCategory=cdc%20fentanyl%20overdose%20v2%20parsed"
+  ),
+  opd = list(
+    queryname = "CDC Opioid Overdose v4 Parsed",
+    apistring = "ccddCategory=cdc%20opioid%20overdose%20v4%20parsed"
+  ),
+  gun1 = list(
+    queryname = "CDC Assault Firearm Injury v1",
+    apistring = "ccddCategory=cdc%20assault%20firearm%20injury%20v1"
+  ),
+  gun2 = list(
+    queryname = "CDC Firearm Injury v2",
+    apistring = "ccddCategory=cdc%20firearm%20injury%20v2"
+  ),
+  traf = list(
+    queryname = "CDC Pedestrian Motor Vehicle Traffic Injury v1",
+    apistring = "ccddCategory=cdc%20pedestrian%20motor%20vehicle%20traffic%20injury%20v1"
+  ),
+  heat = list(
+    queryname = "Heat Related Illness v2",
+    apistring = "ccddCategory=heat%20related%20illness%20v2"
   )
 )
+
+syn <- lapply(syn, \(ls) {
+  ls$apistring <- paste(
+    ls$apistring,
+    "medicalGroupingSystem=essencesyndromes",
+    sep = "&"
+  )
+
+  ls
+})
 
