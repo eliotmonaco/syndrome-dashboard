ce050f07cac90cf4671f9c0b634fce9fa2daf65a
eliotmonaco <eliotmonaco@gmail.com>
2026-02-21 20:32:12 -0600

    rework essence pull and config to use data in satscan

--- scripts/fn.R
+++ scripts/fn.R
@@ -1,57 +1,271 @@
-# Build URLs for Essence API
-build_url <- function(
+# Build URL for Essence API
+build_ess_url <- function(
     syndrome, # list of syndromes built as in `syndromes.R`
-    start, # start date
-    end = Sys.Date(), # end date
-    type = c("ts", "dd")) { # "ts" = time series, "dd" = data details
-  requireNamespace("Rnssp")
+    start, # start date (YYYY-MM-DD)
+    end = Sys.Date(), # end date (YYYY-MM-DD)
+    data_source = c("hospital", "patient"), # data by hospital or patient
+                                            # location
+    output = c("dd", "tb", "ts"), # dd = data details, tb = table builder,
+                                  # ts = time series
+    dd_fields = NULL, # if output is "dd", add desired fields here as character
+                      # vector (e.g., "Date" or "ZipCode")
+    tb_col = NULL, # table builder column (e.g., "timeResolution")
+    tb_rows = NULL, # table builder rows (e.g., "geographyzipcode")
+    zipcodes = FALSE # use zip code free text as geography with zips from kcData
+) {
+  start <- as.Date(start); end <- as.Date(end)
 
-  type <- match.arg(type)
+  if (is.na(start) | is.na(end)) {
+    stop("`start` and `end` must be valid dates formatted YYYY-MM-DD")
+  }
 
-  if (type == "ts") {
-    data <- "timeSeries?"
+  data_source <- match.arg(data_source)
 
-    params1 <- "aqtTarget=TimeSeries"
-  } else if (type == "dd") {
-    data <- "dataDetails/csv?"
+  output <- match.arg(output)
 
-    params1 <- paste(
-      "aqtTarget=DataDetails",
-      "field=Date",
-      "field=Age",
-      "field=Sex",
-      "field=ZipCode",
+  # Output parameters
+  if (output == "dd") {
+    if (is.null(dd_fields)) {
+      stop("When `output` is \"dd\", `dd_fields` cannot be empty")
+    }
+
+    op <- "dataDetails/csv?"
+
+    dd_fields <- paste0("field=", c(dd_fields, "EssenceID"))
+
+    params_out <- paste(
+      c("aqtTarget=DataDetails", dd_fields),
+      collapse = "&"
+    )
+  } else if (output == "tb") {
+    op <- "tableBuilder/csv?"
+
+    if (length("tb_col") > 1) {
+      stop("`tb_col` must have length of 1")
+    }
+
+    if (is.null(tb_col) || is.null(tb_rows)) {
+      stop("When `output` is \"tb\", `tb_col` and `tb_rows` cannot be empty")
+    }
+
+    params_out <- paste(
+      "aqtTarget=TableBuilder",
+      paste0("columnField=", tb_col),
+      paste(paste0("rowFields=", tb_rows), collapse = "&"),
+      sep = "&"
+    )
+  } else if (output == "ts") {
+    op <- "timeSeries?"
+
+    params_out <- "aqtTarget=TimeSeries"
+  }
+
+  # Data source & geography parameters
+  if (data_source == "hospital") {
+    params_ds <- "datasource=va_hosp"
+
+    params_geo <- paste(
+      "geographySystem=hospital",
+      "geography=mochildrensmercyercc",
+      "geography=mochildrensmercynorthlandercc",
+      "geography=moresearchkcercc",
+      "geography=mostlukeskcercc",
+      "geography=mostlukesnorthlandkcercc",
+      "geography=motrumanhospitalhillercc",
+      "geography=motrumanlakewoodercc",
+      "geography=mostjosephkcercc",
+      sep = "&"
+    )
+  } else if (data_source == "patient") {
+    params_ds <- "datasource=va_er"
+
+    params_geo <- paste(
+      "geographySystem=region",
+      "geography=mo_clay",
+      "geography=mo_jackson",
+      "geography=mo_platte",
       sep = "&"
     )
   }
 
-  ep <- "https://moessence.inductivehealth.com/ih_essence/api/"
+  # ZIP code free text
+  if (zipcodes) {
+    params_geo <- paste(
+      "geographySystem=zipcode",
+      paste0(
+        "geography=",
+        paste(sort(unique(unlist(
+          kcData::geoids$zcta
+        ))), collapse = ",")
+      ),
+      sep = "&"
+    )
+  }
 
-  params2 <- paste(
-    "datasource=va_hosp",
+  # Additional parameters
+  params_add <- paste(
     "userId=5809",
-    "geographySystem=hospital",
-    "geography=mochildrensmercyercc",
-    "geography=mochildrensmercynorthlandercc",
-    "geography=moresearchkcercc",
-    "geography=mostlukeskcercc",
-    "geography=mostlukesnorthlandkcercc",
-    "geography=motrumanhospitalhillercc",
-    "geography=motrumanlakewoodercc",
-    "geography=mostjosephkcercc",
-    "startDate=1Jan2025",
-    "endDate=11Sep2025",
+    paste0("startDate=", format(start, "%d%b%Y")),
+    paste0("endDate=", format(end, "%d%b%Y")),
     "percentParam=noPercent",
     "detector=probrepswitch",
     "timeResolution=daily",
     sep = "&"
   )
 
-  params <- paste0(params1, "&", params2)
+  params <- paste(
+    params_out,
+    params_ds,
+    params_geo,
+    params_add,
+    sep = "&"
+  )
 
-  params <- Rnssp::change_dates(params, start_date = start, end_date = end)
+  endpoint <- "https://moessence.inductivehealth.com/ih_essence/api/"
 
-  paste0(ep, data, paste0(params, "&", syndrome))
+  paste0(
+    endpoint,
+    op,
+    paste0(params, "&", syndrome)
+  )
+}
+
+# Wrapper for `Rnssp::get_api_data()` using Data details
+get_ess_dd <- function(url) {
+  Rnssp::get_api_data(
+    url,
+    fromCSV = TRUE,
+    col_types = readr::cols(.default = "c"),
+    name_repair = setmeup::fix_colnames
+  )
+}
+
+# Capture both the message/error and the returned value from a function call
+capture_message <- function(expr) {
+  output <- list()
+
+  withCallingHandlers(
+    expr,
+    error = function(e) {
+      output <<- append(output, list(message = conditionMessage(e)))
+    },
+    message = function(m) {
+      output <<- append(output, list(message = conditionMessage(m)))
+    }
+  )
+
+  output <- append(output, list(data = expr))
+
+  output
+}
+
+# Configure data downloaded from Essence
+config_ess <- function(ess) {
+  # Assign row ID and convert `date` to date
+  ess <- ess |>
+    mutate(
+      row_id = row_number(), .before = 1,
+      date = as.Date(date, "%m/%d/%Y")
+    )
+
+  # Find "exact" duplicates
+  dupes <- suppressMessages(find_dupes(ess, c(
+    "date", "time", "age", "sex", "date_of_birth",
+    "zip_code", "travel", "hospital_name",
+    "visit_number"
+  )))
+
+  # Early return if no dupes are found
+  if (is.null(dupes)) {
+    return(list(
+      data = select(ess, -row_id),
+      error_rate = NULL
+    ))
+  }
+
+  # Remove dupes
+  ess <- ess |>
+    anti_join(dupes, by = "row_id")
+
+  # Deduplicate using `visit_number`
+  dupes <- suppressMessages(find_dupes(ess, "visit_number"))
+
+  # Early return if no dupes are found
+  if (is.null(dupes)) {
+    return(list(
+      data = select(ess, -row_id),
+      error_rate = NULL
+    ))
+  }
+
+  # If members of a dupe set have the same `patient_id`, `date`, and `zip_code`,
+  # all but one are redundant. Only one from each set will be kept in `ess`.
+  ls <- lapply(unique(dupes$dupe_id), \(x) {
+    df <- dupes |> # filter a single dupe set
+      filter(dupe_id == x)
+
+    if (all(
+      length(df$patient_id) == 1,
+      length(df$date) == 1,
+      length(df$zip_code) == 1
+    )) {
+      df[2:nrow(df), ] # redundant dupes to remove
+    }
+  }) |>
+    compact()
+
+  # Remove redundant dupes
+  if (length(ls) > 0) {
+    ess <- ess |> # remove redundant dupes from `ess`
+      anti_join(ls, by = "row_id")
+
+    dupes <- dupes |> # remove redundant dupe sets from `dupes`
+      filter(!dupe_id %in% unique(ls$dupe_id))
+  }
+
+  # Among the remaining dupe sets, if they are indeed duplicates, it isn't
+  # possible to determine which records have the correct date or ZIP code, both
+  # of which are needed for the spatiotemporal analysis. Therefore, all records
+  # will be kept. The potential error rate for both date and ZIP code will be
+  # noted.
+
+  # Find the number of duplicate sets with different values for each variable
+  diffs <- lapply(unique(dupes$dupe_id), \(x) {
+    df <- dupes |> # filter a single dupe set
+      filter(dupe_id == x)
+
+    apply(df, 2, \(c) { # find the number of unique values for each variable
+      length(unique(c))
+    }) |>
+      as.list() |>
+      data.frame() |>
+      mutate(
+        n_dupes = nrow(df),
+        n_potential_errors = n_dupes - 1 # count the number of potential errors
+      )                                  # presuming that one value (date or
+  }) |>                                  # ZIP) is the correct one
+    list_rbind()
+
+  # Replace values > 1 with the number of potential errors. If the value is 1,
+  # no potential errors are counted because the single value is presumed
+  # correct.
+  errors <- diffs |>
+    mutate(across(
+      -c(n_dupes, n_potential_errors),
+      ~ ifelse(.x > 1, n_potential_errors, NA)
+    )) |>
+    select(-c(row_id, dupe_id, n_dupes, n_potential_errors))
+
+  # Sum the potential errors for each variable
+  errors <- colSums(errors, na.rm = TRUE) |>
+    as_tibble(rownames = "var") |>
+    rename(n = value) |>
+    mutate(error_rate = n / nrow(ess))
+
+  list(
+    data = select(ess, -row_id),
+    error_rate = errors
+  )
 }
 
 syn_ggplot <- function(df, alert) {
--- scripts/get-ess.R
+++ scripts/get-ess.R
@@ -1,44 +1,141 @@
-# Get time series and/or data details from Essence via API
+# Get Essence data via API
 
 library(Rnssp)
+library(tidyverse)
+library(setmeup)
 
-# load Essence profile object, needed for `get_api_data()`
+# Load Essence profile object, needed for `get_api_data()`
 load("C:/Users/emonaco01/OneDrive - City of Kansas City/Documents/r/essence/myProfile.rda")
 
-source("scripts/syndromes.R")
 source("scripts/fn.R")
+source("scripts/syndromes.R")
 
-ess <- list()
+# Start date = 1 year and 1 day before current date
+start_date <- Sys.Date() - lubridate::years(1) - lubridate::days(1)
 
-# get time series
-urls <- build_url(syn, start = "2024-01-01", type = "ts")
-
-system.time(
-  ess$ts <- lapply(urls, \(x) {
-    ls <- get_api_data(
-      x,
-      col_types = readr::cols(.default = "c")
-    )
-
-    ls$timeSeriesData
-  })
+# Data details fields
+flds <- c(
+  "Date", "Time", "Age", "Sex", "DateOfBirth",
+  "ZipCode", "Travel", "HospitalName", "VisitNumber",
+  "Patient_ID", "MedicalRecordNumber"
 )
 
-names(ess$ts) <- names(syn)
-
-# get data details
-urls <- build_url(syn, start = "2024-01-01", type = "dd")
-
-system.time(
-  ess$dd <- lapply(urls, \(x) {
-    get_api_data(
-      x,
-      fromCSV = TRUE,
-      col_types = readr::cols(.default = "c")
-    )
-  })
+# Build URLs for data by patient location
+url_pat <- build_ess_url(
+  syndrome = lapply(syn, `[[`, 2),
+  start = start_date,
+  data_source = "patient",
+  output = "dd",
+  dd_fields = flds
 )
 
-names(ess$dd) <- names(syn)
+# Build URLs for data by hospital location
+url_hosp <- build_ess_url(
+  syndrome = lapply(syn, `[[`, 2),
+  start = start_date,
+  data_source = "hospital",
+  output = "dd",
+  dd_fields = flds
+)
 
-saveRDS(ess, "data/ess_raw_data.rds")
+# i <- 1:5
+#
+# url_pat <- url_pat[i]
+# url_hosp <- url_hosp[i]
+#
+# url_pat[[2]] <- sub("startDate=19Feb2025&", "", url_pat[[2]])
+
+# Get data
+t1 <- Sys.time()
+
+ess_pat <- lapply(url_pat, \(x) {
+  capture_message(get_ess_dd(x))
+})
+
+names(ess_pat) <- names(syn)
+
+ess_hosp <- lapply(url_hosp, \(x) {
+  capture_message(get_ess_dd(x))
+})
+
+names(ess_hosp) <- names(syn)
+
+t2 <- Sys.time()
+
+# Create log entry
+qnm <- sapply(syn, `[[`, 1)
+
+qnm <- str_pad(
+  qnm,
+  width = max(nchar(qnm)),
+  side = "right"
+)
+
+msg1 <- sapply(ess_pat, \(ls) {
+  sub("\\n$", "", cli::ansi_strip(ls$message))
+})
+
+msg2 <- sapply(ess_pat, \(ls) {
+  sub("\\n$", "", cli::ansi_strip(ls$message))
+})
+
+logtbl <- data.frame(
+  query = qnm,
+  message1 = msg1,
+  message2 = msg2
+)
+
+colnames(logtbl) <- c(
+  str_pad("SYNDROME QUERY", width = max(nchar(qnm)), side = "right"),
+  str_pad("DATA BY PATIENT", width = max(nchar(msg1)), side = "right"),
+  "DATA BY HOSPITAL"
+)
+
+tf <- tempfile(fileext = ".txt")
+
+write.table(
+  logtbl,
+  file = tf,
+  sep = "\t",
+  quote = FALSE,
+  row.names = FALSE,
+  col.names = TRUE
+)
+
+log <- readLines(tf)
+
+log <- c(
+  paste(
+    "Dashboard updated on",
+    format(Sys.time(), "%Y-%m-%d %I:%M %p"),
+    "\n"
+  ),
+  paste(
+    "Download time:",
+    round_ties_away(as.numeric(t2 - t1), 2),
+    "minutes\n"
+  ),
+  log
+)
+
+writeLines(log, "data/log.txt")
+
+# Configure data
+ess_pat_config <- lapply(ess_pat, \(ls) {
+  tryCatch(
+    config_ess(ls$data),
+    error = function(e) e
+  )
+})
+
+ess_hosp_config <- lapply(ess_hosp, \(ls) {
+  tryCatch(
+    config_ess(ls$data),
+    error = function(e) e
+  )
+})
+
+# Save
+saveRDS(ess_pat_config, "data/data_by_patient.rds")
+saveRDS(ess_hosp_config, "data/data_by_hospital.rds")
+
